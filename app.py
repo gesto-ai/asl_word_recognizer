3cb6223b75fb4bea86ea089b9290944326565159
Daniel Firebanks-Quevedo <dfireban@oberlin.edu>
2022-10-01 11:55:51 -0400

    removed unnecessary imports

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 import streamlit as st
-from numpy import load
-from numpy import expand_dims
-from matplotlib import pyplot as plt
-import cv2
-from PIL import Image, ImageDraw, ImageFont
 import numpy as np
-import os
 import sys
+from torchvision import transforms
 
 sys.path.append("../")
 
-from asl_sign_spelling_classification.train_v0 import * 
+# from asl_sign_spelling_classification.train_v0 import * 
 from model.inception3d import *
 from data_processing.wlasl_videos import *
 
