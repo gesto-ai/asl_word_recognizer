d5260d0877b8bbbb72a6239d53a1e3cf812171a9
Daniel Firebanks-Quevedo <dafirebanks@gmail.com>
2022-10-06 01:41:59 -0400

    Cleaned up predict method in classifier

--- sign_recognizer/word_sign_recognizer.py
+++ sign_recognizer/word_sign_recognizer.py
@@ -42,28 +42,27 @@ class ASLWordRecognizer:
         if num_classes is None:
             num_classes = NUM_CLASSES
         
-        print("loading model")
+        print("Loading model...")
         self.model = load_inception_model(id3_model_path, wlasl_model_path, num_classes)
 
-        print("loading mapping")
+        print("Loading mapping...")
         self.mapping = load_mapping(mapping_path)
 
     @torch.no_grad()
     def predict(self, video_filepath: Union[str, Path]) -> str:
         """Predict/infer word in input video (which can be a file path or url)."""
-        print("processing video")
+        print("Processing video...")
         batched_frames = process_video(video_filepath, 1, 74)
 
-        print("generating predictions")
+        print("Generating predictions...")
         y_pred = self.predict_on_video(batched_frames)
-
         pred_str = convert_y_label_to_string(y=y_pred[-1], mapping=self.mapping)
 
         return pred_str
     
     @torch.no_grad()
     def predict_on_video(self, batched_frames):
-        """
+        """Predict word in video passed as a tensor of frames
         Args:
             model: InceptionI3d
             batched_frames: torch.Tensor: [batch_num, num_channels, num_frames, height, width]
@@ -71,31 +70,21 @@ class ASLWordRecognizer:
             top_prediction: int
         """
 
-        print("step 1")
         # Inference time! Get the logits for the single video example
+        print("-> Step 1: Getting the logits")
         per_frame_logits = self.model(batched_frames)
 
-        # Batch size, number of classes, unclear what the 3rd number/dimension is here?
-        # st.write(f"Logits shape: {per_frame_logits.shape}")
-
         # Predictions from the logits
-        print("step 2")
+        print("-> Step 2: Get the predictions from the logits")
         predictions = torch.max(per_frame_logits, dim=2)[0]
-        # st.write(f"Predictions shape after getting the max per the third dimension: {predictions.shape}")
 
         # Sort predictions in a descending order of relevance - the top prediction is at the end
         # This is an array of indices!
-        print("step 3")
+        print("-> Step 3: Sort predictions from least relevant to most relevant")
         out_labels = np.argsort(predictions.cpu().detach().numpy()[0])
-        # st.write(f"Output predictions: {out_labels}")
 
-        # # Return the top prediction
-        # return out_labels[-1]
-
-        # Return all predictions
-        print("step 4")
-        pred_str = convert_y_label_to_string(y=out_labels[-1], mapping=self.mapping)
-        return pred_str
+        # Return the top prediction
+        return out_labels
 
 def load_mapping(mapping_path):
     mapping = {}
@@ -109,7 +98,6 @@ def load_mapping(mapping_path):
     return mapping
 
 def convert_y_label_to_string(y: np.int64, mapping: Sequence[str]) -> str:
-    # return "".join([mapping[int(i)] for i in y])
     return mapping[y]
 
 def process_video(video_filepath, start_frame, end_frame):
@@ -123,20 +111,20 @@ def process_video(video_filepath, start_frame, end_frame):
     """
     # Load frames
     frames = load_rgb_frames_from_video_dataset(video_filepath, start_frame, end_frame)
-    # st.write(f"Input frames: {type(frames)}, {frames.shape}")
+    # print(f"Input frames: {type(frames)}, {frames.shape}")
 
     # Center video on hand
     img_transform = transforms.Compose([MyCenterCrop(224)])
     transformed_frames = img_transform(frames)
-    # st.write(f"Transformed frames: {type(transformed_frames)}, {transformed_frames.shape}")
+    # print(f"Transformed frames: {type(transformed_frames)}, {transformed_frames.shape}")
 
     # Convert to Torch Tensor and reshape
     tensor_frames = video_to_tensor(transformed_frames)
-    # st.write(f"Tensor frames: {type(tensor_frames)}, {tensor_frames.shape}")
+    # print(f"Tensor frames: {type(tensor_frames)}, {tensor_frames.shape}")
 
     # Add an extra dimension for making this video a part of a batch
     batched_frames = torch.from_numpy(np.expand_dims(tensor_frames, axis=0))
-    # st.write(f"Batched frames: {type(batched_frames)}, {batched_frames.shape}")
+    # print(f"Batched frames: {type(batched_frames)}, {batched_frames.shape}")
 
     return batched_frames
 
