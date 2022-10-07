129640e36e7ae6f17bb8a4fc6eb47a4ada7a8ffb
Ibrahim Sherif <hemamehsherif@gmail.com>
2022-10-08 01:57:10 +0200

    refactored to remove unused code

--- sign_recognizer/model/inception3d.py
+++ sign_recognizer/model/inception3d.py
@@ -1,13 +1,7 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
-
-import numpy as np
-
-import os
-import sys
-from collections import OrderedDict
 
 
 class MaxPool3dSamePadding(nn.MaxPool3d):
@@ -161,32 +155,8 @@ class InceptionI3d(nn.Module):
         http://arxiv.org/pdf/1409.4842v1.pdf.
     """
 
-    # Endpoints of the model in order. During construction, all the endpoints up
-    # to a designated `final_endpoint` are returned in a dictionary as the
-    # second return value.
-    VALID_ENDPOINTS = (
-        'Conv3d_1a_7x7',
-        'MaxPool3d_2a_3x3',
-        'Conv3d_2b_1x1',
-        'Conv3d_2c_3x3',
-        'MaxPool3d_3a_3x3',
-        'Mixed_3b',
-        'Mixed_3c',
-        'MaxPool3d_4a_3x3',
-        'Mixed_4b',
-        'Mixed_4c',
-        'Mixed_4d',
-        'Mixed_4e',
-        'Mixed_4f',
-        'MaxPool3d_5a_2x2',
-        'Mixed_5b',
-        'Mixed_5c',
-        'Logits',
-        'Predictions',
-    )
 
-    def __init__(self, num_classes=400, spatial_squeeze=True,
-                 final_endpoint='Logits', name='inception_i3d', in_channels=3, dropout_keep_prob=0.5):
+    def __init__(self, num_classes=400, name='inception_i3d', in_channels=3):
         """Initializes I3D model instance.
         Args:
           num_classes: The number of outputs in the logit layer (default 400, which
@@ -204,94 +174,66 @@ class InceptionI3d(nn.Module):
           ValueError: if `final_endpoint` is not recognized.
         """
 
-        if final_endpoint not in self.VALID_ENDPOINTS:
-            raise ValueError('Unknown final endpoint %s' % final_endpoint)
+        self.dropout_keep_prob=0.5
 
-        super(InceptionI3d, self).__init__()
+        
+        super().__init__()
         self._num_classes = num_classes
-        self._spatial_squeeze = spatial_squeeze
-        self._final_endpoint = final_endpoint
+        self._spatial_squeeze = True
         self.logits = None
-
-        if self._final_endpoint not in self.VALID_ENDPOINTS:
-            raise ValueError('Unknown final endpoint %s' % self._final_endpoint)
 
         self.end_points = {}
         end_point = 'Conv3d_1a_7x7'
         self.end_points[end_point] = Unit3D(in_channels=in_channels, output_channels=64, kernel_shape=[7, 7, 7],
                                             stride=(2, 2, 2), padding=(3,3,3),  name=name+end_point)
-        if self._final_endpoint == end_point: return
-        
         end_point = 'MaxPool3d_2a_3x3'
         self.end_points[end_point] = MaxPool3dSamePadding(kernel_size=[1, 3, 3], stride=(1, 2, 2),
                                                              padding=0)
-        if self._final_endpoint == end_point: return
-        
         end_point = 'Conv3d_2b_1x1'
         self.end_points[end_point] = Unit3D(in_channels=64, output_channels=64, kernel_shape=[1, 1, 1], padding=0,
                                        name=name+end_point)
-        if self._final_endpoint == end_point: return
-        
         end_point = 'Conv3d_2c_3x3'
         self.end_points[end_point] = Unit3D(in_channels=64, output_channels=192, kernel_shape=[3, 3, 3], padding=1,
                                        name=name+end_point)
-        if self._final_endpoint == end_point: return
-
         end_point = 'MaxPool3d_3a_3x3'
         self.end_points[end_point] = MaxPool3dSamePadding(kernel_size=[1, 3, 3], stride=(1, 2, 2),
                                                              padding=0)
-        if self._final_endpoint == end_point: return
-        
         end_point = 'Mixed_3b'
         self.end_points[end_point] = InceptionModule(192, [64,96,128,16,32,32], name+end_point)
-        if self._final_endpoint == end_point: return
 
         end_point = 'Mixed_3c'
         self.end_points[end_point] = InceptionModule(256, [128,128,192,32,96,64], name+end_point)
-        if self._final_endpoint == end_point: return
 
         end_point = 'MaxPool3d_4a_3x3'
         self.end_points[end_point] = MaxPool3dSamePadding(kernel_size=[3, 3, 3], stride=(2, 2, 2),
                                                              padding=0)
-        if self._final_endpoint == end_point: return
-
         end_point = 'Mixed_4b'
         self.end_points[end_point] = InceptionModule(128+192+96+64, [192,96,208,16,48,64], name+end_point)
-        if self._final_endpoint == end_point: return
-
+        
         end_point = 'Mixed_4c'
         self.end_points[end_point] = InceptionModule(192+208+48+64, [160,112,224,24,64,64], name+end_point)
-        if self._final_endpoint == end_point: return
 
         end_point = 'Mixed_4d'
         self.end_points[end_point] = InceptionModule(160+224+64+64, [128,128,256,24,64,64], name+end_point)
-        if self._final_endpoint == end_point: return
-
+        
         end_point = 'Mixed_4e'
         self.end_points[end_point] = InceptionModule(128+256+64+64, [112,144,288,32,64,64], name+end_point)
-        if self._final_endpoint == end_point: return
-
+        
         end_point = 'Mixed_4f'
         self.end_points[end_point] = InceptionModule(112+288+64+64, [256,160,320,32,128,128], name+end_point)
-        if self._final_endpoint == end_point: return
 
         end_point = 'MaxPool3d_5a_2x2'
         self.end_points[end_point] = MaxPool3dSamePadding(kernel_size=[2, 2, 2], stride=(2, 2, 2),
-                                                             padding=0)
-        if self._final_endpoint == end_point: return
-
+                                                             padding=0)        
         end_point = 'Mixed_5b'
         self.end_points[end_point] = InceptionModule(256+320+128+128, [256,160,320,32,128,128], name+end_point)
-        if self._final_endpoint == end_point: return
 
         end_point = 'Mixed_5c'
         self.end_points[end_point] = InceptionModule(256+320+128+128, [384,192,384,48,128,128], name+end_point)
-        if self._final_endpoint == end_point: return
 
         end_point = 'Logits'
-        self.avg_pool = nn.AvgPool3d(kernel_size=[2, 7, 7],
-                                     stride=(1, 1, 1))
-        self.dropout = nn.Dropout(dropout_keep_prob)
+        self.avg_pool = nn.AvgPool3d(kernel_size=[2, 7, 7], stride=(1, 1, 1))
+        self.dropout = nn.Dropout(self.dropout_keep_prob)
         self.logits  = Unit3D(in_channels=384+384+128+128, output_channels=self._num_classes,
                              kernel_shape=[1, 1, 1],
                              padding=0,
@@ -301,6 +243,11 @@ class InceptionI3d(nn.Module):
                              name='logits')
 
         self.build()
+
+
+    def build(self):
+        for k in self.end_points.keys():
+            self.add_module(k, self.end_points[k])
 
 
     def replace_logits(self, num_classes):
@@ -313,30 +260,12 @@ class InceptionI3d(nn.Module):
                              use_bias=True,
                              name='logits')
 
-    def build(self):
-        for k in self.end_points.keys():
-            self.add_module(k, self.end_points[k])
-        
-    def forward(self, x, pretrained=False, n_tune_layers=-1):
-        if pretrained:
-            assert n_tune_layers >= 0
 
-            freeze_endpoints = self.VALID_ENDPOINTS[:-n_tune_layers]
-            tune_endpoints = self.VALID_ENDPOINTS[-n_tune_layers:]
-        else:
-            freeze_endpoints = []
-            tune_endpoints = self.VALID_ENDPOINTS
-
-        # backbone, no gradient part
-        with torch.no_grad():
-            for end_point in freeze_endpoints:
-                if end_point in self.end_points:
-                    x = self._modules[end_point](x) # use _modules to work with dataparallel
+    def forward(self, x):
 
         # backbone, gradient part
-        for end_point in tune_endpoints:
-            if end_point in self.end_points:
-                x = self._modules[end_point](x) # use _modules to work with dataparallel
+        for end_point in self.end_points:
+            x = self._modules[end_point](x) # use _modules to work with dataparallel
 
         # head
         x = self.logits(self.dropout(self.avg_pool(x)))
@@ -344,10 +273,3 @@ class InceptionI3d(nn.Module):
             logits = x.squeeze(3).squeeze(3)
         # logits is batch X time X classes, which is what we want to work with
         return logits
-        
-
-    def extract_features(self, x):
-        for end_point in self.VALID_ENDPOINTS:
-            if end_point in self.end_points:
-                x = self._modules[end_point](x)
-        return self.avg_pool(x)
