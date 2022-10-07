3ea76f916c9162497f901e6d4eb7cec37ce40f75
Ibrahim Sherif <hemamehsherif@gmail.com>
2022-10-08 00:47:45 +0200

    added datasets for preparing data

---
+++ datasets/nslt_dataset.py
@@ -0,0 +1,158 @@
+import json
+import os
+#import os.path
+import random
+
+import cv2
+import numpy as np
+import torch
+import torch.utils.data as data_utl
+
+from datasets.preprocessing import load_rgb_frames_from_video, video_to_tensor
+
+
+def make_dataset(split_file, split, vid_root, num_classes):
+    dataset = []
+    with open(split_file, 'r') as f:
+        data = json.load(f)
+
+    i = 0
+    count_skipping = 0
+    for vid in data.keys():
+        if split == 'train':
+            if data[vid]['subset'] not in ['train', 'val']:
+                continue
+        else:
+            if data[vid]['subset'] != 'test':
+                continue
+
+        src = 0
+
+        video_path = os.path.join(vid_root, vid + '.mp4')
+        if not os.path.exists(video_path):
+            continue
+
+        num_frames = int(cv2.VideoCapture(video_path).get(cv2.CAP_PROP_FRAME_COUNT))
+
+        if num_frames - 0 < 9:
+            print("Skip video ", vid)
+            count_skipping += 1
+            continue
+
+        label = np.zeros((num_classes, num_frames), np.float32)
+
+        for l in range(num_frames):
+            c_ = data[vid]['action'][0]
+            label[c_][l] = 1
+
+        if len(vid) == 5:
+            dataset.append((vid, label, src, 0, data[vid]['action'][2] - data[vid]['action'][1]))
+        elif len(vid) == 6:  ## sign kws instances
+            dataset.append((vid, label, src, data[vid]['action'][1], data[vid]['action'][2] - data[vid]['action'][1]))
+
+        i += 1
+    print("Skipped videos: ", count_skipping)
+    print(len(dataset))
+    return dataset
+
+
+def get_num_class(split_file):
+    classes = set()
+
+    content = json.load(open(split_file))
+
+    for vid in content.keys():
+        class_id = content[vid]['action'][0]
+        classes.add(class_id)
+
+    return len(classes)
+
+
+class NSLT(data_utl.Dataset):
+
+    def __init__(self, split_file, split, root, transforms=None):
+        self.num_classes = get_num_class(split_file)
+
+        self.data = make_dataset(split_file, split, root, num_classes=self.num_classes)
+        self.split_file = split_file
+        self.transforms = transforms
+        self.root = root
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is class_index of the target class.
+        """
+        vid, label, src, start_frame, nf = self.data[index]
+
+        total_frames = 64
+
+        try:
+            start_f = random.randint(0, nf - total_frames - 1) + start_frame
+        except ValueError:
+            start_f = start_frame
+
+        imgs = load_rgb_frames_from_video(self.root, vid, start_f, total_frames)
+
+        imgs, label = self.pad(imgs, label, total_frames)
+
+        imgs = self.transforms(imgs)
+
+        ret_lab = torch.from_numpy(label)
+        ret_img = video_to_tensor(imgs)
+
+        return ret_img, ret_lab, vid
+
+    def __len__(self):
+        return len(self.data)
+
+    def pad(self, imgs, label, total_frames):
+        if imgs.shape[0] < total_frames:
+            num_padding = total_frames - imgs.shape[0]
+
+            if num_padding:
+                prob = np.random.random_sample()
+                if prob > 0.5:
+                    pad_img = imgs[0]
+                    pad = np.tile(np.expand_dims(pad_img, axis=0), (num_padding, 1, 1, 1))
+                    padded_imgs = np.concatenate([imgs, pad], axis=0)
+                else:
+                    pad_img = imgs[-1]
+                    pad = np.tile(np.expand_dims(pad_img, axis=0), (num_padding, 1, 1, 1))
+                    padded_imgs = np.concatenate([imgs, pad], axis=0)
+        else:
+            padded_imgs = imgs
+
+        label = label[:, 0]
+        label = np.tile(label, (total_frames, 1)).transpose((1, 0))
+
+        return padded_imgs, label
+
+    @staticmethod
+    def pad_wrap(imgs, label, total_frames):
+        if imgs.shape[0] < total_frames:
+            num_padding = total_frames - imgs.shape[0]
+
+            if num_padding:
+                pad = imgs[:min(num_padding, imgs.shape[0])]
+                k = num_padding // imgs.shape[0]
+                tail = num_padding % imgs.shape[0]
+
+                pad2 = imgs[:tail]
+                if k > 0:
+                    pad1 = np.array(k * [pad])[0]
+
+                    padded_imgs = np.concatenate([imgs, pad1, pad2], axis=0)
+                else:
+                    padded_imgs = np.concatenate([imgs, pad2], axis=0)
+        else:
+            padded_imgs = imgs
+
+        label = label[:, 0]
+        label = np.tile(label, (total_frames, 1)).transpose((1, 0))
+
+        return padded_imgs, label
+
