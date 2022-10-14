e94fffa0d64d984a74d55d1271b361e2f0d5f41a
Saroramath <67832595+Saroramath@users.noreply.github.com>
2022-10-14 01:48:58 -0230

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ uploaded_video = st.file_uploader("Upload a video...")
 if uploaded_video is not None:
     # We'll need this path for opening the video with OpenCV
     
-    video_filepath = f"sign-recognizer/new-videos/{uploaded_video.name}"        
+    video_filepath = f"sign-recognizer/new-videos/{uploaded_video.name}"    
+    S3Video_URL = f"https://sign-recognizer.s3.amazonaws.com/new-videos/{uploaded_video.name}"
     print(f"Video filepath: {video_filepath}")
 
     # Save video to disk
@@ -32,21 +33,21 @@ if uploaded_video is not None:
         f.write(uploaded_video.read()) 
 
     # Open video from disk path - technically not needed because we can feed the bytes-like object to st.video
-    st_video = open(f"https://sign-recognizer.s3.amazonaws.com/new-videos/{uploaded_video.name}", 'rb')
-    video_bytes = st_video.read()
-    st.video(video_bytes)
+    #st_video = open(f"https://sign-recognizer.s3.amazonaws.com/new-videos/{uploaded_video.name}", 'rb')
+    #video_bytes = st_video.read()
+    st.video(S3Video_URL)
     st.write("Uploaded video and stored to disk!")
 
     if AWS_LAMBDA_URL is None:
         st.write("AWS Lambda URL not found. Initializing model with local code...")
         model = PredictorBackend()
-        video_url = video_filepath
+        video_url = S3Video_URL
     else:
         st.write("AWS Lambda URL found! Initializing model with predictor backend...")
         model = PredictorBackend(url=AWS_LAMBDA_URL)
         # Since we don't have a custom URL for the uploaded video, we'll temporarily use the demo video 
         st.write("NOTE: Since we don't have a custom URL for the uploaded video, we'll temporarily return the prediction for the demo video instead!")
-        video_url = DEMO_VIDEO_URL
+        video_url = S3Video_URL
     
     st.write("Getting prediction...")
     prediction = model.run(video_url)
